03f25a8078e8b6b986df23f4b3e96f2b9b8797ba
Sarah Mount <sarah.mount@dxw.com>
2023-03-31 16:18:56 +0100

    install-bin also installs data files

--- script/install-bin.sh
+++ script/install-bin.sh
@@ -4,6 +4,7 @@ set -o errexit
 set -o nounset
 
 BIN_DIR="${HOME}/bin"
+DATA_DIR="${HOME}/data"
 TOP_LEVEL_DIR=$(git rev-parse --show-toplevel)
 
 if [[ ! -L "${BIN_DIR}" || ! -d  "${BIN_DIR}" ]]
@@ -13,4 +14,13 @@ then
     ln -s "${TOP_LEVEL_DIR}/bin" "${BIN_DIR}"
 else
     echo "${BIN_DIR} is a symlink."
+fi
+
+if [[ ! -L "${DATA_DIR}" || ! -d  "${DATA_DIR}" ]]
+then
+    echo "${DATA_DIR} is NOT a symlink."
+    rm -f "${DATA_DIR}"
+    ln -s "${TOP_LEVEL_DIR}/data" "${DATA_DIR}"
+else
+    echo "${DATA_DIR} is a symlink."
 fi
