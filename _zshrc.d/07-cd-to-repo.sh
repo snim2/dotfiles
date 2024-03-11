05713bfd6fc345a94a5a808af6f66bb712e8eaf0
Sarah Mount <sarah.mount@dxw.com>
2024-03-11 16:21:06 +0000

    'repo -' goes to the last visited repository
    * Add a history stack to the repo script.
    * Expose 'popr', 'pushr' and 'repos' which are like 'popd',
      'pushd' and 'dirs' for the 'cd' command.
    * Save repo history in '$HOME/.zsh_repo_history' which has a
      maximum of 10000 lines (one line per repo).
    * Expose an env var called 'OLD_REPO' which is like 'OLDPWD'.
    * Save repo history when we exit the shell.
    * 'repo -' acts like 'cd -'.
    
    Caveats:
    
    * This only works consistently if you always use 'repo' consistently,
      if you just 'cd' to a repo directory then the stack that stores repo
      paths will not be altered.
    * The code here might have been neater if arrays had not been used.

--- _zshrc.d/07-cd-to-repo.sh
+++ _zshrc.d/07-cd-to-repo.sh
@@ -6,17 +6,81 @@
 #
 # Use like this:
 #     repo dotfiles
+#     repo -
 #
-repo() {
-    if [[ $1 = "-" ]];
-        then cd - || return 2
+
+declare -a REPO_HISTORY
+
+REPO_HISTORY_MAX_FILE_SIZE=10000
+REPO_HISTORY_FILE="$HOME/.zsh_repo_history"
+OLD_REPO=""
+
+__load_repo_history() {
+   if [[ ! -f "$REPO_HISTORY_FILE" ]];
+    then
+        return 0
     fi
-    DIRECTORY="$(find "$HOME/code" -type d -name "$1" -maxdepth 4 -mindepth 3 -exec test -d {}/.git \; -print | head -n 1)"
+    local history_size=0
+    while IFS= read -r line;
+    do
+        REPO_HISTORY+=("$line")
+        history_size=$((history_size+1))
+        if [[ $history_size -gt $REPO_HISTORY_MAX_FILE_SIZE ]];
+        then
+            break
+        fi
+    done < "$REPO_HISTORY_FILE"
+    return 0
+}
+
+__load_repo_history
+
+__save_repo_history() {
+    touch "$REPO_HISTORY_FILE"
+    local truncated_repos=("${REPO_HISTORY[@]:0:$REPO_HISTORY_MAX_FILE_SIZE}")
+    printf "%s\n" "${truncated_repos[@]}" > "$REPO_HISTORY_FILE"
+}
+
+trap '__save_repo_history' EXIT
+
+popr() {
+    if [ ${#REPO_HISTORY[@]} -eq 0 ];
+    then
+        echo ""
+        return 0
+    fi
+    local last_index=$((${#REPO_HISTORY[@]} - 1))
+    local last_repo=${REPO_HISTORY[last_index]}
+    unset 'REPO_HISTORY[last_index]'
+    REPO_HISTORY=("${REPO_HISTORY[@]}")
+    OLD_REPO="$(echo "$last_repo" | tr -d '[:space:]')"
+    echo "$OLD_REPO"
+}
+
+pushr() {
+    local repo="$1"
+    REPO_HISTORY+=("$repo")
+}
+
+repos() {
+    printf '%s\n' "${REPO_HISTORY[@]}"
+}
+
+repo() {
+    local new_directory=""
+    if [[ $1 == "-" ]];
+    then
+        new_directory=$(popr)
+    else
+        new_directory="$(find "$HOME/code" -type d -name "$1" -maxdepth 4 -mindepth 3 -exec test -d {}/.git \; -print | head -n 1)"
+    fi
+    pushr "$new_directory"
     # We could just say cd $(...) || return 1, but because Zsh is configured to
     # print something to the console after every invocation of 'cd', this is
     # quite annoying.
-    if [[ -z "$DIRECTORY" ]];
-        then return 1
+    if [[ -z "$new_directory" ]];
+    then
+        return 1
     fi
-    cd "$DIRECTORY" || return 2
+    cd "$new_directory" || return 2
 }
