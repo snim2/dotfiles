5261fd8fee78b811ff39b22b7260b0f44fcaabbf
Sarah Mount <sarah.mount@dxw.com>
2024-03-11 12:57:03 +0000

    Repo command accepts - as an argument
    Previously 'repo -' failed becauase the command could
    not find a directory that was a Git repository called '-'.
    
    This commit changes that behaviour, so that 'repo -'
    just runs 'cd' to the last directory that was used as
    the CWD. Note that this will allow 'repo' to cd to
    a directory which was not a Git repository.

--- _zshrc.d/07-cd-to-repo.sh
+++ _zshrc.d/07-cd-to-repo.sh
@@ -8,6 +8,9 @@
 #     repo dotfiles
 #
 repo() {
+    if [[ $1 = "-" ]];
+        then cd - || return 2
+    fi
     DIRECTORY="$(find "$HOME/code" -type d -name "$1" -maxdepth 4 -mindepth 3 -exec test -d {}/.git \; -print | head -n 1)"
     # We could just say cd $(...) || return 1, but because Zsh is configured to
     # print something to the console after every invocation of 'cd', this is
