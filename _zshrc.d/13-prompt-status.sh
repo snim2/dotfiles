95d35e84bd9f1c548657b3936152c99e14aa1091
Sarah Mount <mount.sarah@gmail.com>
2026-05-06 19:18:00 +0100

    Add previous cmd ret code and timing to prompt

---
+++ _zshrc.d/13-prompt-status.sh
@@ -0,0 +1,30 @@
+# Must run before other precmd hooks to capture $? from the last user command.
+_prompt_precmd_capture() {
+    _PROMPT_EXIT_STATUS=$?
+    if [[ -n ${_PROMPT_CMD_START-} ]]; then
+        _PROMPT_DURATION=$(( EPOCHREALTIME - _PROMPT_CMD_START ))
+    else
+        _PROMPT_DURATION=0
+    fi
+    unset _PROMPT_CMD_START
+}
+
+_prompt_preexec() {
+    _PROMPT_CMD_START=$EPOCHREALTIME
+}
+
+# Must run after Starship's precmd to override the empty RPROMPT it sets.
+_prompt_set_rprompt() {
+    local code=${_PROMPT_EXIT_STATUS:-0}
+    local dur_str
+    dur_str=$(printf '%.1fs' "${_PROMPT_DURATION:-0}")
+    if (( code == 0 )); then
+        RPROMPT="%F{green}${code}%f (${dur_str})"
+    else
+        RPROMPT="%F{red}${code}%f (${dur_str})"
+    fi
+}
+
+preexec_functions+=(_prompt_preexec)
+precmd_functions=(_prompt_precmd_capture "${precmd_functions[@]}")
+precmd_functions+=(_prompt_set_rprompt)
